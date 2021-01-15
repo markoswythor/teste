615b719bca2c16cbdea0ef166bb65d6cbd1e7b69
markoswythor <76796419+markoswythor@users.noreply.github.com>
2021-01-15 19:23:38 -0300

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -1,92 +1,146 @@
 const fetch = require('node-fetch')
-const FormData = require('form-data')
-const { MessageType } = require('@adiwajshing/baileys')
+const imgbb = require('imgbb-uploader')
+const axios = require('axios')
+const cfonts = require('cfonts')
+const spin = require('spinnies')
+const Crypto = require('crypto')
 
-let handler  = async (m, { conn, args }) => {
-  let q = m.quoted ? { message: { [m.quoted.mtype]: m.quoted }} : m
-  if (/image/.test((m.quoted ? m.quoted : m).mtype)) {
-    let img = await conn.downloadM(q)
-    if (!img) throw img
-    let stiker = await sticker(img)
-    conn.sendMessage(m.chat, stiker, MessageType.sticker, {
-      quoted: m
-    })
-  } else if (args[0]) {
-    let stiker = await sticker(false, args[0])
-    conn.sendMessage(m.chat, stiker, MessageType.sticker, {
-      quoted: m
-    })
-  }
-}
-handler.help = ['stiker (caption|reply media)', 'stiker <url>']
-handler.tags = ['sticker']
-handler.command = /^stic?ker$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
+const wait = async (media) => new Promise(async (resolve, reject) => {
+    const attachmentData = `data:image/jpeg;base64,${media.toString('base64')}`
+    const response = await fetch("https://trace.moe/api/search",{method: "POST",body: JSON.stringify({ image: attachmentData }),headers: { "Content-Type": "application/json" }});
+    if (!response.ok) reject(`Gambar tidak ditemukan!`);
+    const result = await response.json()
+    try {
+    	const { is_adult, title, title_chinese, title_romaji, title_english, episode, season, similarity, filename, at, tokenthumb, anilist_id } = result.docs[0]
+    	let belief = () => similarity < 0.89 ? "Saya memiliki keyakinan rendah dalam hal ini : " : ""
+    	let ecch = () => is_adult ? "Iya" : "Tidak"
+    	resolve({video: await getBuffer(`https://media.trace.moe/video/${anilist_id}/${encodeURIComponent(filename)}?t=${at}&token=${tokenthumb}`), teks: `${belief()}
+~> Ecchi : *${ecch()}*
+~> Judul Jepang : *${title}*
+~> Ejaan Judul : *${title_romaji}*
+~> Judul Inggris : *${title_english}*
+~> Episode : *${episode}*
+~> Season  : *${season}*`});
+	} catch (e) {
+		console.log(e)
+		reject(`Saya tidak tau ini anime apa`)
+	}
+})
 
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-
-module.exports = handler
-
-async function canvas(code, type = 'png', quality = 0.92) {
-    let res = await fetch('https://nurutomo.herokuapp.com/api/canvas?' + queryURL({
-        type,
-        quality
-    }), {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'text/plain',
-            'Content-Length': code.length
-        },
-        body: code
-    })
-    let image = await res.buffer()
-    return image
+const simih = async (text) => {
+	try {
+		const sami = await fetch(`https://secureapp.simsimi.com/v1/simsimi/talkset?uid=297971048&av=6.9.3.4&lc=id&cc=ID&tz=Asia%2FJakarta&os=a&ak=quS%2FxiW%2Bb8ys5agzpljUdoPdLH8%3D&message_sentence=${text}&normalProb=8&isFilter=1&talkCnt=1&talkCntTotal=1&reqFilter=1&session=nSt8PSSmKRbcR7quUkfhXYpmDYgErtBefVbkkri9CrGSVjm4Cj2e2zBFjvdxSijp56WjyK6g2EWTj3KxKz65DL22&triggerKeywords=%5B%5D`, {method: 'GET'})
+		const res = await sami.json()
+		return res.simsimi_talk_set.answers[0].sentence
+	} catch {
+		return 'Simi ga tau kak'
+	}
 }
 
-function queryURL(queries) {
-    return Object.entries(queries).map(([key, value]) => key + (value ? '=' + encodeURIComponent(value) : '')).join('&')
+const h2k = (number) => {
+    var SI_POSTFIXES = ["", " K", " M", " G", " T", " P", " E"]
+    var tier = Math.log10(Math.abs(number)) / 3 | 0
+    if(tier == 0) return number
+    var postfix = SI_POSTFIXES[tier]
+    var scale = Math.pow(10, tier * 3)
+    var scaled = number / scale
+    var formatted = scaled.toFixed(1) + ''
+    if (/\.0$/.test(formatted))
+      formatted = formatted.substr(0, formatted.length - 2)
+    return formatted + postfix
 }
 
-let { fromBuffer } = require('file-type')
-async function sticker(img, url) {
-    url = url ? url : await uploadImage(img)
-    let {
-        mime
-    } = url ? {mime:'image/jpeg'} : await fromBuffer(img)
-    let sc = `let im = await loadImg('data:${mime};base64,'+(await window.loadToDataURI('${url}')))
-c.width = c.height = 512
-let max = Math.max(im.width, im.height)
-let w = 512 * im.width / max
-let h = 512 * im.height / max
-ctx.drawImage(im, 256 - w / 2, 256 - h / 2, w, h)
-`
-    return await canvas(sc, 'webp')
+const getBuffer = async (url, options) => {
+	try {
+		options ? options : {}
+		const res = await axios({
+			method: "get",
+			url,
+			headers: {
+				'DNT': 1,
+				'Upgrade-Insecure-Request': 1
+			},
+			...options,
+			responseType: 'arraybuffer'
+		})
+		return res.data
+	} catch (e) {
+		console.log(`Error : ${e}`)
+	}
 }
 
-function uploadImage(buffer) {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const {
-                ext
-            } = await fromBuffer(buffer)
-            let form = new FormData()
-            form.append('file', buffer, 'tmp.' + ext)
-            let res = await fetch('https://telegra.ph/upload', {
-                method: 'POST',
-                body: form
-            })
-            let img = await res.json()
-            if (img.error) reject(img.error)
-            else resolve('https://telegra.ph' + img[0].src)
-        } catch (e) {
-            reject(e)
-        }
-    })
+const randomBytes = (length) => {
+    return Crypto.randomBytes(length)
 }
+
+const generateMessageID = () => {
+    return randomBytes(10).toString('hex').toUpperCase()
+}
+
+const getGroupAdmins = (participants) => {
+	admins = []
+	for (let i of participants) {
+		i.isAdmin ? admins.push(i.jid) : ''
+	}
+	return admins
+}
+
+const getRandom = (ext) => {
+	return `${Math.floor(Math.random() * 10000)}${ext}`
+}
+
+const spinner = { 
+  "interval": 120,
+  "frames": [
+    "🕐",
+    "🕑",
+    "🕒",
+    "🕓",
+    "🕔",
+    "🕕",
+    "🕖",
+    "🕗",
+    "🕘",
+    "🕙",
+    "🕚",
+    "🕛"
+  ]}
+
+let globalSpinner;
+
+
+const getGlobalSpinner = (disableSpins = false) => {
+  if(!globalSpinner) globalSpinner = new spin({ color: 'blue', succeedColor: 'green', spinner, disableSpins});
+  return globalSpinner;
+}
+
+spins = getGlobalSpinner(false)
+
+const start = (id, text) => {
+	spins.add(id, {text: text})
+		/*setTimeout(() => {
+			spins.succeed('load-spin', {text: 'Suksess'})
+		}, Number(wait) * 1000)*/
+	}
+const info = (id, text) => {
+	spins.update(id, {text: text})
+}
+const success = (id, text) => {
+	spins.succeed(id, {text: text})
+
+	}
+
+const close = (id, text) => {
+	spins.fail(id, {text: text})
+}
+
+const banner = cfonts.render(('WHATSAPP|BOT'), {
+    font: 'chrome',
+    color: 'candy',
+    align: 'center',
+    gradient: ["red","yellow"],
+    lineHeight: 3
+  });
+
+
+module.exports = { wait, simih, getBuffer, h2k, generateMessageID, getGroupAdmins, getRandom, start, info, success, banner, close }
